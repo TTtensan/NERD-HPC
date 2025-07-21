82d2e4ed0aabaf4084520bbf14ed2c3ad15fb2ba
TTtensan <43802833+TTtensan@users.noreply.github.com>
2025-07-22 02:10:07 +0900

    expand array

--- software/src/basic.cpp
+++ software/src/basic.cpp
@@ -35,7 +35,7 @@
 #define SIZE_LINE 64 //Command line buffer length + NULL
 #define SIZE_IBUF 64 //i-code conversion buffer size
 #define SIZE_LIST 32768 //List buffer size
-#define SIZE_ARRY 32 //Array area size
+#define SIZE_ARRY 64 //Array area size
 #define SIZE_GSTK 20 //GOSUB stack size(2/nest)
 #define SIZE_LSTK 50 //FOR stack size(5/nest)
 
